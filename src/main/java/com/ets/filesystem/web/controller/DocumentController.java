1a1afb7131cfc2f1ee66492ee0d8dc149323479f
skose.vbt <serhat.kose@vbt.com.tr>
2022-02-18 14:54:41 +0300

    File Upload API Operations

--- src/main/java/com/ets/filesystem/web/controller/DocumentController.java
+++ src/main/java/com/ets/filesystem/web/controller/DocumentController.java
@@ -2,12 +2,20 @@ package com.ets.filesystem.web.controller;
 
 import com.ets.filesystem.service.*;
 import com.ets.filesystem.web.model.*;
+import com.ets.filesystem.web.model.File;
+import org.apache.tomcat.jni.*;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.core.io.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.*;
+import org.springframework.web.servlet.mvc.method.annotation.*;
 
 import java.io.*;
+import java.net.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.*;
 
 @RestController
 @RequestMapping("api/v1/document")
@@ -20,11 +28,6 @@ public class DocumentController {
         this.storageService = storageService;
     }
 
-    @GetMapping("/hello")
-    public ResponseEntity<?> getHello(){
-
-        return ResponseEntity.ok().body("HELLO DOCUMENT PROJECT");
-    }
 
     @PostMapping(value = "/upload",consumes = MediaType.MULTIPART_FORM_DATA_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
@@ -39,5 +42,16 @@ public class DocumentController {
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
         }
 
+    }
+    @GetMapping("/files")
+    public ResponseEntity<List<File>> getListFiles() {
+        return ResponseEntity.status(HttpStatus.OK).body(storageService.getAllFiles());
+    }
+
+    @GetMapping("/files/{filename}")
+    public ResponseEntity<Resource> getFile(@PathVariable String filename){
+        Resource file = storageService.loadFile(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
 }
